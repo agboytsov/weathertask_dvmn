183645fcb27ce0d89e36b6c30a3c0c99d537b4fe
agboytsov <133656833+agboytsov@users.noreply.github.com>
2023-08-16 09:56:13 +0300

    Update main.py
    Final version

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
-import os
 import requests
 
 
-
-def get_weather_from_wttr(city: str = 'san francisco') -> str:
+def get_weather_from_wttr(city: str='san francisco') -> str:
     """Function gets information about weather in specified city/place.
     The result is in Russian, metrical, black-and-white format"""
     link = 'https://wttr.in/'
     payload = {
-        'lang': 'ru',
-        'm': '',
-        'M': '',
-        'n': '',
-        'q': '',
-        'T': ''
+      'lang' : 'ru', 
+      'm' : '', 
+      'M' : '',  
+      'n' :'', 
+      'q' : '', 
+      'T' : ''
     }
-    try:
-        response = requests.get(f'{link}{city}', params=payload)
-        response.raise_for_status()
-        return response.text
 
-    except Exception as ex:
-        return f'Произошла ошибка:\n {ex}'  # The result can be printed or stored somehow else
+    response = requests.get(f'{link}{city}', params = payload)
+    response.raise_for_status()
+    return response.text
+  
+    
+if __name__ == "__main__":
+    for place in ['london','svo','череповец']:
+        try:
+            print(get_weather_from_wttr(place))
+        except (requests.ConnectionError, requests.HTTPError) as ex:
+            print(f'Произошла ошибка:\n {ex}')
+  
